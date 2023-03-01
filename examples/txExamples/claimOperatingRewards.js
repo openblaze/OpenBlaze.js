4fa374e6fdf8c0ad52df20482a30a66749388248
ducksquaddd <ducksquadddd@gmail.com>
2023-03-01 09:31:49 -0800

    Rebrand + Some changes

---
+++ examples/txExamples/claimOperatingRewards.js
@@ -0,0 +1,23 @@
+const OpenBlaze = require('../../build/index');
+const config = require('../config.json');
+(async () => {
+  // Private key into a Keypair (PrivateKey and Public key)
+  let Vkey = OpenBlaze.PrivToKeypair('QXmXFQb-8q2MQd_PvHIxJArvH2izyxu4CKrCarqtCX8');
+
+  const client = new OpenBlaze.Client({
+    node: config.node,
+    Keypair: Vkey,
+  });
+
+  const transaction = {
+    transactionType: 'claimOperatingRewards',
+    Data: {
+      time: Date.now(),
+    },
+  };
+
+  console.log(transaction);
+  let res = await client.broadcast(transaction);
+
+  console.log(res);
+})();
